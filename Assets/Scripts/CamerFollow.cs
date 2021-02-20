839c421f6478d3abe9aeee6df0926d9ec019dd70
jacobsonj <justinjacobson0118@gmail.com>
2021-02-19 21:35:01 -0700

    made more like actual game for testing

---
+++ Assets/Scripts/CamerFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerFollow : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+
+    void Start()
+    {
+        // offset = new Vector3()
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-﻿using Mirror;
+﻿// using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +8,16 @@ using System.Text;
 // using System.Web.Script.Serialization;
 
 
-public class Player : NetworkBehaviour
+public class Player : MonoBehaviour
 {
+    public Rigidbody rb;
+    public Vector3 direction;
+    public float moveSpeed = 10;
+    public float rotateSpeed = 10;
     private long time = 0;
     private static readonly HttpClient client = new HttpClient();
 
-    [SyncVar(hook = nameof(onHolaCountChange))]
-    int holaCount = 0;
+    
 
     void Start()
     {
@@ -23,22 +26,27 @@ public class Player : NetworkBehaviour
     async void HandleMovement()
     {
         
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+        // Vector3 Movement = new Vector3(moveHorizontal * 0.1f, 0, moveVertical * 0.1f);
+        // transform.position = transform.position + Movement;
+        
+        
+        // if(Movement == new Vector3(0,0,0) )
+        // {
+        //     return;
+        // }
 
-        if (!isLocalPlayer)
+        direction = new Vector3(horizontalMove, 0.0f, verticalMove);
+
+        if (direction != Vector3.zero)
         {
-            return;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotateSpeed * Time.deltaTime);
         }
-        
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 Movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
-        transform.position = transform.position + Movement;
-        
-        
-        if(Movement == new Vector3(0,0,0) )
-        {
-            return;
-        }
+
+        rb.MovePosition(transform.position + moveSpeed * Time.deltaTime * direction);
+
+
         if(time == 0)
         {
             time = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
@@ -104,53 +112,8 @@ public class Player : NetworkBehaviour
     async void Update()
     {
         HandleMovement();
-        if(isLocalPlayer && Input.GetKeyDown("x"))
-        {
-            // var posx = transform.position.x.ToString();
-            // var posy = transform.position.y.ToString();
-            // var posz = transform.position.z.ToString();
-            
-            // var rotx = transform.position.x.ToString();
-            // var roty = transform.position.y.ToString();
-            // var rotz = transform.position.z.ToString();
-
-            // var values = new Dictionary<string, string>
-            // {
-            //     { "posx", posx },
-            //     { "posy", posy },
-            //     { "posz", posz },
-            //     { "rotx", rotx },
-            //     { "roty", roty },
-            //     { "rotz", rotz }
-            // };
-            // print(transform.position);
-            
-            // var content = new FormUrlEncodedContent(values);
-
-            // var response = await client.PostAsync("http://localhost:7000/", content);
-
-            // var responseString = await response.Content.ReadAsStringAsync();
-        }
     }
 
-    [TargetRpc]
-    void replyHola()
-    {
-        print("recieved hola from server");
-    }
-
-    [Command]
-    void hola()
-    {
-        print("recieved hola from client");
-        holaCount += 1;
-        replyHola();
-    }
-
-    void onHolaCountChange(int oldHolaCount, int newHolaCount)
-    {
-        print($"Hola count was {oldHolaCount} but now we have {newHolaCount}");
-    }
 }
 
 [Serializable]
